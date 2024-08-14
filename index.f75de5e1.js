2b01cf057881fffc6838593ed8da52220eb727f5
AndriiZhovtko <andriy.zhovtko@gmail.com>
2024-08-14 15:36:09 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const populationElements = document.querySelectorAll(".population");
+const populations = Array.from(populationElements).map((el)=>el.textContent);
+const populationNumbers = populations.map((pop)=>{
+    const number = parseFloat(pop.replace(/,/g, ""));
+    return isNaN(number) ? 0 : number;
+});
+const totalPopulation = populationNumbers.reduce((acc, num)=>acc + num, 0);
+const averagePopulation = totalPopulation / populationNumbers.length;
+function formatNumber(number) {
+    return number.toLocaleString();
+}
+const totalPopulationElement = document.querySelector(".total-population");
+if (totalPopulationElement) totalPopulationElement.textContent = formatNumber(totalPopulation);
+const averagePopulationElement = document.querySelector(".average-population");
+if (averagePopulationElement) averagePopulationElement.textContent = formatNumber(averagePopulation);
+
+//# sourceMappingURL=index.f75de5e1.js.map
